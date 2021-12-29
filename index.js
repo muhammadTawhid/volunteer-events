d1aa6ba296db1dcc203d003b235546e7a88397d5
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-12-29 19:33:43 +0600

    crud opration and heroku practice

--- index.js
+++ index.js
@@ -50,6 +50,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
